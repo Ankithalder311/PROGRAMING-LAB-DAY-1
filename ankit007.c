98724ce06d2c246277942a361a9f3cfcadff3ca3
ANKIT HALDER <ankit0007507@gmail.com>
2025-08-19 16:39:09 +0530

    Add files via upload

---
+++ ankit007.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h.>
+
+int main(){
+
+    int p,r,t;
+    printf("enter principle=",p);
+    scanf("%d",&p);
+    printf("enter rate of interest=",r);
+    scanf("%d",&r);
+    printf("enter time period=",t);
+    scanf("%d",&t);
+    printf("the simple interest is =%d",(p*r*t)/100);
+
+        return 0;
+
+    }
